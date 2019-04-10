017d6c6c152e0c822d0ea1e9604098234458ea2c
AD <atar.dci@gmail.com>
2019-04-10 17:21:15 +0200

    Import Clock component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 
 export default class Header extends Component {
-  
-    render() {
-      return (
-        <header>
-          {/* weather widget */}
-          <span className="fas fa-cog">&nbsp;</span>
-        </header>
-      );
-    }
+
+  render() {
+    return (
+      <header>
+        <Clock />
+        {/* weather widget */}
+        <span className="fas fa-cog">&nbsp;</span>
+      </header>
+    );
+  }
 
 }
