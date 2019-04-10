8383889a0fa75869c4ec91b707321e5233af264a
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-10 12:47:58 +0200

    Add legal notice and privacy policy

---
+++ src/components/PrivacyPolicy.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class PrivacyPolicy extends Component {
+      
+    render() {
+      return (
+        <article>
+          <h1>Datenschutzerklärung (Legal Notice)</h1> 
+
+          </article>
+      );
+    }
+
+}
