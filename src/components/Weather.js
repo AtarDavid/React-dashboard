7c2d8d4ccaf4a7e64f46b024b6331bf2267ebf9d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-16 01:09:32 +0200

    Add weather widget
    Show weather icon and temperature from Berlin (12681)

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 export default class Weather extends Component {
-  
+
+    componentDidMount () {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?zip=12681,de&units=metric&appid=6ae8d6fa95479985fa2f3d6c41820fbd`)
+          .then(
+              response => response.json()
+          )
+          .then(
+              weather => {
+                  document.querySelector(`.weather .icon`).innerHTML = `<img src="../assets/img/weather/${weather.weather[0].icon}.svg">`
+                  document.querySelector(`.weather .temperature`).innerHTML = Math.round(weather.main.temp) + '°C'
+              },
+          )
+          .catch(
+              err => document.querySelector(`.weather`).innerHTML = `An error has occurred.`
+          );
+    }
+
     render() {
       return (
         <div className="weather">
-            { /* weather */ }
+          <div className="icon" />
+          <div className="temperature" />
         </div>
       );
     }
