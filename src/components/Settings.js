fa395166cbe8bf97d9438344a5f271af455e1686
AD <atar.dci@gmail.com>
2019-04-16 00:33:47 +0200

    handleChange-mAdd two list items Change each item's value to keyword onChange event listener

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react';
 
 export default class Settings extends Component {
-  
-    render() {
-      return (
-        <React.Fragment>
 
-          <div id="settings" className="settings">
+  handleChange = (event) => {
+    console.log("change theme")
+  }
 
-            <a href="#" className="settings-toggle-out">
-              <span className="fas fa-times">{'\u00A0'}</span>
-            </a>
+  render() {
+    return (
+      <React.Fragment>
 
-            <h2>Settings</h2>
+        <div id="settings" className="settings">
 
-            <div className="username-input"><span>User</span> <input id="usernameInput" type="text" placeholder="Enter your name" /></div>
-            
-            <ul className="background-categories">
-                <li><label>{/* img */}<br /><input type="radio" name="background" value="1" /></label></li>
-                <li><label>{/* img */}<br /><input type="radio" name="background" value="2" /></label></li>
-                <li><label>{/* img */}<br /><input type="radio" name="background" value="3" /></label></li>
-                <li><label>{/* img */}<br /><input type="radio" name="background" value="4" /></label></li>
-                <li><label>{/* img */}<br /><input type="radio" name="background" value="5" /></label></li>
-                <li><label>{/* img */}<br /><input type="radio" name="background" value="6" /></label></li>
-            </ul>
+          <a href="#" className="settings-toggle-out">
+            <span className="fas fa-times">{'\u00A0'}</span>
+          </a>
 
-            <div className="zip-input"><span>ZIP</span><input id="zipCodeInput" type="number" placeholder="12345" /></div>
-          
-          </div>
+          <h2>Settings</h2>
 
-          <a href="#" className="backdrop"></a>
+          <div className="username-input"><span>User</span> <input id="usernameInput" type="text" placeholder="Enter your name" /></div>
 
-        </React.Fragment>
-      );
-    }
+          <ul className="background-categories">
+            <li><label>{/* img */}<br /><input type="radio" name="background" value="landscape" onChange={this.handleChange} /></label></li>
+            <li><label>{/* img */}<br /><input type="radio" name="background" value="paris" onChange={this.handleChange} /></label></li>
+            <li><label>{/* img */}<br /><input type="radio" name="background" value="space" onChange={this.handleChange} /></label></li>
+            <li><label>{/* img */}<br /><input type="radio" name="background" value="cityscape" onChange={this.handleChange} /></label></li>
+            <li><label>{/* img */}<br /><input type="radio" name="background" value="wildlife" onChange={this.handleChange} /></label></li>
+            <li><label>{/* img */}<br /><input type="radio" name="background" value="ocean" onChange={this.handleChange} /></label></li>
+            <li><label>{/* img */}<br /><input type="radio" name="background" value="blackwhite" onChange={this.handleChange} /></label></li>
+            <li><label>{/* img */}<br /><input type="radio" name="background" value="architecture" onChange={this.handleChange} /></label></li>
+          </ul>
+
+          <div className="zip-input"><span>ZIP</span><input id="zipCodeInput" type="number" placeholder="12345" /></div>
+
+        </div>
+
+        <a href="#" className="backdrop"></a>
+
+      </React.Fragment>
+    );
+  }
 
 }
