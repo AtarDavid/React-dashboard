0c6c962e43b1b7d26a76a1ccd856b5524a709136
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-18 16:01:45 +0200

    Add thumbnails of categories of background images

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -17,14 +17,14 @@ export default class Settings extends Component {
           <div className="username-input"><span>User</span> <input id="usernameInput" type="text" placeholder="Enter your name" onKeyDown={this.props.changeUsername} /></div>
 
           <ul className="background-categories">
-            <li><label>{/* img */}<br /><input type="radio" name="background" value="landscape" onChange={this.props.handleChange} /></label></li>
-            <li><label>{/* img */}<br /><input type="radio" name="background" value="paris" onChange={this.props.handleChange} /></label></li>
-            <li><label>{/* img */}<br /><input type="radio" name="background" value="space" onChange={this.props.handleChange} /></label></li>
-            <li><label>{/* img */}<br /><input type="radio" name="background" value="cityscape" onChange={this.props.handleChange} /></label></li>
-            <li><label>{/* img */}<br /><input type="radio" name="background" value="wildlife" onChange={this.props.handleChange} /></label></li>
-            <li><label>{/* img */}<br /><input type="radio" name="background" value="ocean" onChange={this.props.handleChange} /></label></li>
-            <li><label>{/* img */}<br /><input type="radio" name="background" value="blackwhite" onChange={this.props.handleChange} /></label></li>
-            <li><label>{/* img */}<br /><input type="radio" name="background" value="architecture" onChange={this.props.handleChange} /></label></li>
+            <li><label><img src={require(`../assets/img/background/landscape.jpg`)} /><br /><input type="radio" name="background" value="landscape" onChange={this.props.handleChange} /></label></li>
+            <li><label><img src={require(`../assets/img/background/paris.jpg`)} /><br /><input type="radio" name="background" value="paris" onChange={this.props.handleChange} /></label></li>
+            <li><label><img src={require(`../assets/img/background/space.jpg`)} /><br /><input type="radio" name="background" value="space" onChange={this.props.handleChange} /></label></li>
+            <li><label><img src={require(`../assets/img/background/cityscape.jpg`)} /><br /><input type="radio" name="background" value="cityscape" onChange={this.props.handleChange} /></label></li>
+            <li><label><img src={require(`../assets/img/background/wildlife.jpg`)} /><br /><input type="radio" name="background" value="wildlife" onChange={this.props.handleChange} /></label></li>
+            <li><label><img src={require(`../assets/img/background/ocean.jpg`)} /><br /><input type="radio" name="background" value="ocean" onChange={this.props.handleChange} /></label></li>
+            <li><label><img src={require(`../assets/img/background/blackwhite.jpg`)} /><br /><input type="radio" name="background" value="blackwhite" onChange={this.props.handleChange} /></label></li>
+            <li><label><img src={require(`../assets/img/background/architecture.jpg`)} /><br /><input type="radio" name="background" value="architecture" onChange={this.props.handleChange} /></label></li>
           </ul>
 
           <div className="zip-input"><span>ZIP</span><input id="zipCodeInput" type="number" placeholder="12345" /></div>
