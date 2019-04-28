e4ef1c2d29ef09c7a4d39edd1f875230c899c216
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-28 14:13:42 +0200

    Add NewsFeed component
    Create a new file 'NewsFeed.js' and add component 'NewsFeed';
    import NewsFeed in TabMenu.js

---
+++ src/components/NewsFeed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class NewsFeed extends Component {
+  
+  render() {
+    return (
+      <div className="newsfeed">
+        { /* Newsfeed */ }
+      </div>
+    );
+  }
+
+}
