67c0bdcb5f99dc3761cdc8cc11850822ebdcf91e
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-28 15:05:12 +0200

    Add newsfeed (fetch from 'News API')
    Show list items with titles (links to source websites), sources and descriptions of the news

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 export default class NewsFeed extends Component {
-  
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://newsapi.org/v2/top-headlines?country=de&apiKey=02bb62ce0f84446fb6d2039e7adeea31')
+      .then(response => response.json())
+      .then(data => this.setState({ articles: data.articles }));
+  }
+
   render() {
+    const { articles } = this.state;
+
     return (
       <div className="newsfeed">
-        { /* Newsfeed */ }
+        <ul>
+          {articles.map(news =>
+            <li>
+              <a className="newsfeed-title" target="_blanc" href={news.url}>{news.title}</a> <span className="newsfeed-source">({news.source.name})</span>
+              <p className="newsfeed-description">{news.description}</p>
+            </li>
+          )}
+        </ul>
       </div>
     );
   }
